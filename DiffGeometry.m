e9a28d6c5f64c718e15d12efa63d10f84c1ac7f2
g3n1uss <g3n1uss@gmail.com>
2016-02-13 14:17:53 -0500

    Added several functions/comments and one more example

--- DiffGeometry.m
+++ DiffGeometry.m
@@ -5,8 +5,13 @@ BeginPackage["DiffGeometry`"]
 CrisSymb::usage = "Conns[gdown] returns Christoffel symbols with first upper index, gdown - covariant metric tensor";
 Riemann::usage = "Riemann[connect] returns Riemann tensor with first upper index, connect - Christoffel symbols with first upper index";
 Ricci::usage = "Ricci[Riemann] returns scalar curvature and Ricci tensor, Riemann - Riemann tensor with first upper index";
-x::usage = "Coordinates should be defined globally "
+SpinConn::usage = "SpinConn[Frames, timeposition, assumptions] retunrs spin connections, Frames - frames with first upper Lorentz index and second lower and spacetime index; timeposition - position of the time direction; assumptions - assumptions for simplifications "
+x::usage = "Coordinates should be defined globally, see examples "
 Begin["`Private`"]
+
+(* =========================================== *)
+
+(* calculate Christoffel symbols, first index is upper, last two are lower *)
 
 CrisSymb[gdown_]:=Module[{ii, jj, kk, ll, dimension = Length[gdown], gder, crisdown, 
     connect, gup = FullSimplify[Inverse[gdown]]},
@@ -28,6 +33,8 @@ CrisSymb[gdown_]:=Module[{ii, jj, kk, ll, dimension = Length[gdown], gder, crisd
    
    connect = 1/2 Simplify[gup.crisdown]; connect];
 
+(* =========================================== *)
+
 (* Riemann tensor, first index is upper *)
 
 Riemann[connect_] := 
@@ -43,6 +50,10 @@ Riemann[connect_] :=
         dimension}], {ga, 1, dimension}, {al, 1, dimension}, {be, 1, 
      dimension}, {rho, 1, dimension}]; Simplify[Riem]];
 
+(* =========================================== *)
+
+(* Ricci tensor *)
+
 Ricci[Riemann_, gup_] := 
   Module[{kk, ii, jj, dimension = Length[Riemann], Ric, Curv},
    Ric = ConstantArray[0, {dimension, dimension}];
@@ -51,11 +62,43 @@ Ricci[Riemann_, gup_] :=
      Simplify[
       Sum[Riemann[[kk, ii, kk, jj]], {kk, 1, dimension}]], {ii, 1, 
      dimension}, {jj, 1, dimension}];
-   (* calculating curvature *)
-   
-   Curv = Simplify[
+
+(* calculating curvature, Ricci scalar *)
+    Curv = Simplify[
      Sum[Ric[[ii, jj]]*gup[[ii, jj]], {ii, 1, dimension}, {jj, 1, 
        dimension}]]; {Curv, Ric}];
+
+(* =========================================== *)
+
+(* calculate spin connections *)
+(*input:frames with first upper Lorentz index and second lower and spacetime index; tpos - position of the time direction; assumps - some assumptions for simplifications *)
+
+SpinConn[Frames_, tpos_, assumps_] := 
+  Module[{dim = Length[Frames], Omega, gup, gdown, eta, mu, nu, mm, 
+    nn, aa, bb, rho, sig, ee}, eta = IdentityMatrix[dim];
+   gdown = ConstantArray[0, {dim, dim}];
+   Omega = ConstantArray[0, {dim, dim, dim}]; eta[[tpos, tpos]] = -1;
+   Do[gdown[[mu, nu]] = 
+     Simplify[
+      Sum[Frames[[mm, mu]] eta[[mm, nn]] Frames[[nn, nu]], {mm, 1, 
+        dim}, {nn, 1, dim}], assumps], {mu, 1, dim}, {nu, 1, dim}];
+   gup = Simplify[Inverse[gdown], assumps];
+   Print["gup is done"];
+   Do[Omega[[mu, aa, bb]] = 
+     Sum[(Frames.gup)[[aa, 
+         nu]] (D[Frames[[bb, nu]], x[mu]] - 
+          D[Frames[[bb, mu]], x[nu]]), {nu, 1, dim}] - 
+      Sum[(Frames.gup)[[bb, 
+         nu]] (D[Frames[[aa, nu]], x[mu]] - 
+          D[Frames[[aa, mu]], x[nu]]), {nu, 1, dim}] - 
+      Sum[(Frames.gup)[[aa, rho]] (Frames.gup)[[bb, 
+         sig]] (D[Frames[[ee, sig]], x[rho]] - 
+          D[Frames[[ee, rho]], x[sig]]) (eta.Frames)[[ee, mu]], {rho, 
+        1, dim}, {sig, 1, dim}, {ee, 1, dim}], {mu, 1, dim}, {aa, 1, 
+     dim}, {bb, 1, dim}]; {gdown, gup, 1/2 Omega}];
+
+(* =========================================== *)
+
 
 End[]
 
